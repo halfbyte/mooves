2298f8c093e00d0afb41dcad1dd05511ccc7199d
Jan Krutisch <jan@krutisch.de>
2009-03-23 22:38:44 +0100

    Your dailly Tactics board update
    - Changed Behaviour to context sensitive (no need to switch modes anymore)
    - Added Arrowheads to arrows (should be configurable so that you can draw lines)
    - Fixed a few ugly fugly firefox bugs

--- javascripts/tacticsboard.js
+++ javascripts/tacticsboard.js
@@ -15,6 +15,10 @@ function Arrow(x,y,xEnd, yEnd) {
   this.y = y
   this.yEnd = yEnd
   this.finished = false
+  this.hoverStart = false
+  this.hoverEnd = false
+  this.boundStart = false
+  this.boundEnd = false
 }
 
 var board = {
@@ -25,6 +29,7 @@ var board = {
   mouseY: 0,
   imageLoadCount: 0,
   imageCount: 2,
+  context: null,
   init: function() {
     var self = this;
     this.images.player_a = new Image();
@@ -33,8 +38,8 @@ var board = {
     this.images.player_b.src = "images/player_b.png"
     var canvas = $('#board')[0]
     var ctx = canvas.getContext("2d");
-
-    this.players[this.players.length] = new Player(10,10,Math.PI*(45/180), this.images.player_a);
+    this.context = ctx;
+    this.players[this.players.length] = new Player(200,200,Math.PI*(45/180), this.images.player_a);
     this.players[this.players.length] = new Player(100,100,Math.PI*(75/180), this.images.player_b);
     // players[players.length] = new Player(20,20,0.4);
     this.images.player_a.onload = function() {
@@ -66,14 +71,41 @@ var board = {
   
   initEventHandlers: function() {
     var self = this;
+    
+    $(window).keydown(function(event){
+      if (event.keyCode == 18) {
+        self.tool = 'turn';
+        self.setToolbar('turn');
+      }
+        
+      
+    });
+    
+    $(window).keyup(function(event){
+      if (event.keyCode == 18) {
+        self.tool = 'move'
+        self.setToolbar('move');
+      }
+      
+    });
+    
+    
     $('#board').mouseup(function(e) {
+      var anyPlayerBound = false
       jQuery.each(self.players, function(i, player) {
+        if (player.bound) anyPlayerBound = true;
         player.bound = false
       })
-      if (self.tool == 'arrow') {
+      if (!anyPlayerBound) {
         jQuery.each(self.arrows, function(i,arrow) {
           if (!arrow.finished) {
             arrow.finished = true
+          }
+          if (arrow.boundStart) {
+            arrow.boundStart = false
+          }
+          if (arrow.boundEnd) {
+            arrow.boundEnd = false
           }
         })        
       }
@@ -83,32 +115,58 @@ var board = {
     $('#board').mousemove(function(e) {
       var x = (e.pageX - $('#board').offset().left)
       var y = (e.pageY - $('#board').offset().top)
+      var anyPlayerBound = false;
       jQuery.each(self.players, function(i, player) {
         if (player.bound) {
+          anyPlayerBound = true
           if (self.tool == 'move') {
             player.x = x
             player.y = y            
           } else if(self.tool == 'turn') {
             var diff = self.mouseY - y
-            console.log(diff)
             player.rot -= diff / 20.0
             self.mouseY = y
           }
         } else {
-          if (x > (player.x - 7) && x < (player.x + 7) && y > (player.y - 7) && y < (player.y) + 7) {
+          if (self.isInBounds(x,y,player.x,player.y,7)) {
             player.hover = true
           } else {
             player.hover = false
           }
         }
       })
-      if (self.tool == 'arrow') {
+      if (!anyPlayerBound) {
+        var anyArrowUnfinished = false
+        
         jQuery.each(self.arrows, function(i,arrow) {
           if (!arrow.finished) {
+            anyArrowUnfinished = true
             arrow.xEnd = x;
             arrow.yEnd = y;
           }
         })
+        if (!anyArrowUnfinished) {
+          jQuery.each(self.arrows, function(i,arrow) {
+            if (arrow.boundStart) {
+              arrow.x = x
+              arrow.y = y 
+            } else if (arrow.boundEnd) {
+              arrow.xEnd = x
+              arrow.yEnd = y               
+            } else {
+              if (self.isInBounds(x,y,arrow.x,arrow.y,5)) {
+                arrow.hoverStart = true
+              } else {
+                arrow.hoverStart = false
+              }
+              if (self.isInBounds(x,y,arrow.xEnd,arrow.yEnd,5)) {
+                arrow.hoverEnd = true
+              } else {
+                arrow.hoverEnd = false
+              }
+            }
+          })
+        }
       }
       self.draw();
     })
@@ -117,21 +175,36 @@ var board = {
       var x = (e.pageX - $('#board').offset().left)
       var y = (e.pageY - $('#board').offset().top)
       self.mouseY = y
-      if (self.tool == 'move' || self.tool == 'turn') {
-        jQuery.each(self.players, function(i, player) {
-          if (x > (player.x - 7) && x < (player.x + 7) && y > (player.y - 7) && y < (player.y) + 7) {
-            player.bound = true        
-          }
-        })        
-      } else if (self.tool == 'arrow') {
+      
+      var matchedPlayer = false;
+      
+      jQuery.each(self.players, function(i, player) {
+        if (self.isInBounds(x,y,player.x,player.y,7)) {
+          player.bound = true
+          matchedPlayer = true
+        }
+      })        
+      if (!matchedPlayer) {
+        var anyArrowBound = false
         jQuery.each(self.arrows, function(i,arrow) {
           if (!arrow.finished) {
             arrow.finished = true
           }
+          if (!anyArrowBound) {
+            if (arrow.hoverStart) {
+              arrow.boundStart = true
+              anyArrowBound = true
+            }
+            if (arrow.hoverEnd) {
+              arrow.boundEnd = true
+              anyArrowBound = true
+            }            
+          }
         })
-        var arrow = new Arrow(x,y,x,y);
-        console.log(arrow)
-        self.arrows[self.arrows.length] = arrow
+        if (!anyArrowBound) {
+          var arrow = new Arrow(x,y,x,y);
+          self.arrows[self.arrows.length] = arrow          
+        }
       }
       self.draw();
     })
@@ -141,7 +214,6 @@ var board = {
   checkLoadStatus: function() {
     var self = this;
     var status = self.imageLoadCount / self.imageCount
-    console.log("status:" + status)
     if (status >= 1) {
       self.draw();
       self.initEventHandlers();
@@ -153,49 +225,89 @@ var board = {
   
   setToolbar: function (tool) {
     $('ul#toolbar li a').removeClass('active')
-    console.log('a#tool-' + tool)
     $('a#tool-' + tool).addClass('active')
   },
   draw: function() {
-    var canvas = $('#board')[0]
-    var ctx = canvas.getContext("2d");
+    //var canvas = $('#board')[0]
+    var ctx = this.context
     var self = this
     //ctx.globalCompositeOperation = 'destination-over';
     ctx.clearRect(0,0,600,400); // clear canvas
     jQuery.each(self.arrows, function(i,arrow) {
       ctx.save();
-      ctx.lineWidth = 4
-      if (arrow.finished) {
-        ctx.strokeStyle = '#ffffff'
-      } else {
-        ctx.strokeStyle = '#ffff00'
-      }
-      ctx.beginPath();
-      ctx.moveTo(arrow.x,arrow.y);
-      ctx.lineTo(arrow.xEnd,arrow.yEnd);
-      ctx.stroke();
-        
+        ctx.lineWidth = 4
+        if (arrow.finished) {
+          ctx.strokeStyle = '#ffffff'
+          ctx.fillStyle = "#ffffff"
+        } else {
+          ctx.strokeStyle = '#ffff00'
+          ctx.fillStyle = "#ffff00"
+        }
+        ctx.beginPath();
+        ctx.moveTo(arrow.x,arrow.y);
+        ctx.lineTo(arrow.xEnd,arrow.yEnd);
+        ctx.stroke();
+
+        ctx.save();
+          ctx.lineWidth = 2;
+          var angle;
+          ctx.translate(arrow.xEnd,arrow.yEnd);
+          if ((arrow.xEnd == arrow.x)) {
+            angle = (arrow.yEnd - arrow.y > 0) ? (Math.PI) : 0
+          } else if ((arrow.xEnd - arrow.x) >= 0) {
+            angle = Math.atan((arrow.yEnd - arrow.y) / (arrow.xEnd - arrow.x)) + (Math.PI / 2)
+          } else {
+            angle = Math.atan((arrow.yEnd - arrow.y) / (arrow.xEnd - arrow.x)) - (Math.PI / 2)
+          }
+          if (isNaN(angle)) {
+                                angle = 0;
+                              }
+          ctx.rotate(angle);
+          ctx.translate(-10,-5);
+          ctx.beginPath();
+          ctx.moveTo(10,0)
+          ctx.bezierCurveTo(10,5,18,20,20,20);
+          //ctx.lineTo(20,20)
+          ctx.lineTo(0,20);
+          //ctx.lineTo(10,0);
+          ctx.bezierCurveTo(2,20,10,5,10,0);
+          //ctx.closePath();
+          ctx.fill();
+        ctx.restore();
+      
+        if (arrow.hoverStart) {
+          ctx.fillStyle = arrow.boundStart ? "#ffff00" : "#ff0000"
+          ctx.fillRect(arrow.x-5, arrow.y-5, 10, 10)
+        }
+        if (arrow.hoverEnd) {
+          ctx.fillStyle = arrow.boundEnd ? "#ffff00" : "#ff0000"
+          ctx.fillRect(arrow.xEnd-5, arrow.yEnd-5, 10, 10)
+        }
+      ctx.restore();
     })
 
     jQuery.each(self.players, function(i, player) {
       ctx.save();
-      ctx.translate(player.x,player.y)
+        ctx.translate(player.x,player.y)
 
-      ctx.rotate(player.rot);
+        ctx.rotate(player.rot);
 
-      ctx.lineWidth = 2
-      ctx.drawImage(player.image, -20,-10);
-      ctx.rotate(-player.rot);
-      if (player.bound) {
-        ctx.strokeStyle = "#ffff00"
-        ctx.strokeRect(-7,-7,14,14) 
-        
-      } else if (player.hover) {
-        ctx.strokeStyle = "#ff0000"
-        ctx.strokeRect(-7,-7,14,14) 
-      }
+        ctx.lineWidth = 2
+        ctx.drawImage(player.image, -20,-10);
+        ctx.rotate(-player.rot);
+        if (player.bound) {
+          ctx.strokeStyle = "#ffff00"
+          ctx.strokeRect(-7,-7,14,14) 
+      
+        } else if (player.hover) {
+          ctx.strokeStyle = "#ff0000"
+          ctx.strokeRect(-7,-7,14,14) 
+        }
       ctx.restore();
     })
+  },
+  isInBounds: function(xEvent, yEvent, xTest, yTest, bounds) {
+    return (xEvent > (xTest - bounds) && xEvent < (xTest + bounds) && yEvent > (yTest - bounds) && yEvent < (yTest) + bounds)
   }
 }
 
